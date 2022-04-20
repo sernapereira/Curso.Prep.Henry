667dd09570bd277ad58901d50f2596da633a21f8
sernapereira <101057373+sernapereira@users.noreply.github.com>
2022-04-20 13:36:16 -0500

    frist commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "Hola como estas";
 const nuevoNum = 7;
 
 // Crea una variable booleana:
-const nuevoBool = true;
+const nuevoBool =  true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
