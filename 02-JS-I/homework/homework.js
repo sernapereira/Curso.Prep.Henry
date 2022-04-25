8dc0c31b103ab03d2312e2aecf419d253e636e88
sernapereira <101057373+sernapereira@users.noreply.github.com>
2022-04-25 12:41:14 -0500

    12- 25/04/22 12:41

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -10,7 +10,7 @@ const nuevoNum = 7;
 const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta =  10 - 5 === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * 4 === 40 ;
