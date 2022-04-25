299b6ec1053f2b1c375b977dda95af807571aa78
sernapereira <101057373+sernapereira@users.noreply.github.com>
2022-04-25 13:20:47 -0500

    170+ 25/04/22 1:20pm

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -170,12 +170,8 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-<<<<<<< HEAD
   var numeroAleatorio = Math.random();
   return numeroAleatorio;
-=======
->>>>>>> 5ccbbdd3b67a2199c49fe29b7396ba1fabae316a
-
 }
 
 function esPositivo(numero) {
