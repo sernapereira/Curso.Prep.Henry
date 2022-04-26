4e1885d94b7082ddf84e96e93b1d65484c82f298
sernapereira <101057373+sernapereira@users.noreply.github.com>
2022-04-26 11:38:18 -0500

    107+ 26/04/22 11:38am

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -112,6 +112,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  return numero % 1 === 0;
 }
 
 function fizzBuzz(numero) {
